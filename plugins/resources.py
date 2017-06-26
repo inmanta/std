b0991da5a99424e955f534cd0936007ae7b008f0
Bart Vanbrabant <bart.vanbrabant@inmanta.com>
2017-06-26 20:14:57 +0200

    Add agent config resource type

--- plugins/resources.py
+++ plugins/resources.py
@@ -17,13 +17,14 @@
 """
 
 import logging
+import os
 import re
 import urllib
-import os
 
-from inmanta.agent.handler import provider, ResourceHandler, HandlerContext
+from inmanta import data
+from inmanta.agent.handler import provider, ResourceHandler, HandlerContext, CRUDHandler, ResourcePurged
 from inmanta.execute.util import Unknown
-from inmanta.resources import Resource, PurgeableResource, resource, ResourceNotFoundExcpetion
+from inmanta.resources import Resource, PurgeableResource, resource, ResourceNotFoundExcpetion, IgnoreResourceException
 
 
 LOGGER = logging.getLogger(__name__)
@@ -93,6 +94,24 @@ class Symlink(PurgeableResource):
         A symbolic link on the filesystem
     """
     fields = ("source", "target", "reload")
+
+
+@resource("std::AgentConfig", agent="agent", id_attribute="agentname")
+class AgentConfig(PurgeableResource):
+    """
+        A resource that can modify the agentmap for autostarted agents
+    """
+    fields = ("agentname", "uri", "autostart")
+
+    @staticmethod
+    def get_autostart(exp, obj):
+        try:
+            if not obj.autostart:
+                raise IgnoreResourceException()
+        except Exception as e:
+            # When this attribute is not set, also ignore it
+            raise IgnoreResourceException()
+        return obj.autostart
 
 
 @provider("std::File", name="posix_file")
@@ -507,3 +526,43 @@ class SymlinkProvider(ResourceHandler):
             self._io.remove(resource.target)
             self._io.symlink(resource.source, resource.target)
             ctx.set_updated()
+
+
+@provider("std::AgentConfig", name="agentrest")
+class AgentConfigHandler(CRUDHandler):
+    def _get_map(self) -> dict:
+        def call():
+            return self.get_client().get_setting(tid=self._agent.environment, id=data.AUTOSTART_AGENT_MAP)
+
+        value = self.run_sync(call)
+        return value.result["value"]
+
+    def _set_map(self, agent_config: dict) -> None:
+        def call():
+            return self.get_client().set_setting(tid=self._agent.environment, id=data.AUTOSTART_AGENT_MAP, value=agent_config)
+
+        return self.run_sync(call)
+
+    def read_resource(self, ctx: HandlerContext, resource: AgentConfig) -> None:
+        agent_config = self._get_map()
+        ctx.set("map", agent_config)
+
+        if resource.agentname not in agent_config:
+            raise ResourcePurged()
+
+        resource.uri = agent_config[resource.agentname]
+
+    def create_resource(self, ctx: HandlerContext, resource: AgentConfig) -> None:
+        agent_config = ctx.get("map")
+        agent_config[resource.agentname] = resource.uri
+        self._set_map(agent_config)
+
+    def delete_resource(self, ctx: HandlerContext, resource: AgentConfig) -> None:
+        agent_config = ctx.get("map")
+        del agent_config[resource.agentname]
+        self._set_map(agent_config)
+
+    def update_resource(self, ctx: HandlerContext, changes: dict, resource: AgentConfig) -> None:
+        agent_config = ctx.get("map")
+        agent_config[resource.agentname] = resource.uri
+        self._set_map(agent_config)
