935b84fd8df5866237b9439200bf87563ab09d5b
Wouter De Borger <wouter.deborger@inmanta.com>
2018-11-22 12:29:17 +0100

    updated for root user

--- Jenkinsfile
+++ Jenkinsfile
@@ -18,7 +18,7 @@ pipeline {
       steps{
         script{
           withCredentials([usernamePassword(credentialsId: 'jenkins_on_openstack', passwordVariable: 'OS_PASSWORD', usernameVariable: 'OS_USERNAME')]) {
-            sh "vagrant ssh -c 'cd std; ~/venv/bin/python3 -m pytest tests --junitxml=junit.xml'"
+            sh "vagrant ssh -c 'cd std; /opt/venv/bin/python3 -m pytest tests --junitxml=junit.xml'"
             sh "vagrant ssh-config >ssh.conf"
             sh "scp -F ssh.conf default:std/junit.xml ."
           }
--- vagrant/centos_test_setup.sh
+++ vagrant/centos_test_setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -x
 
-sudo yum install -y epel-release
-sudo yum update -y
-sudo yum install -y git python34 python34-setuptools python34-pip python34-devel
+yum install -y epel-release
+yum update -y
+yum install -y git python34 python34-setuptools python34-pip python34-devel
 
 ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa
-sudo cat ~/.ssh/id_rsa.pub >>/root/.ssh/authorized_keys 
+cat ~/.ssh/id_rsa.pub >>/root/.ssh/authorized_keys 
 
-python3 -m venv ~/venv
+python3 -m venv /opt/venv
 
-~/venv/bin/python3 -m pip install --upgrade pip
-~/venv/bin/python3 -m pip install inmanta pytest-inmanta 
+/opt/venv/bin/python3 -m pip install --upgrade pip
+/opt/venv/bin/python3 -m pip install inmanta pytest-inmanta 
