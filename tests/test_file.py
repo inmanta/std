5c2a261bff233be43811b4d0171c6a1f13a57d0b
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2019-05-13 15:12:49 +0200

    Add dryruns to test cases.

--- tests/test_file.py
+++ tests/test_file.py
@@ -31,12 +31,20 @@ std::ConfigFile(host=host, path="%(path1)s", content=std::file("unittest/testfil
 
     file1 = project.get_resource("std::ConfigFile", path=test_path_1)
     assert file1.path == test_path_1
+
+    ctx_dryrun1 = project.dryrun(file1)
+    assert ctx_dryrun1.changes["purged"]["current"]
+    assert not ctx_dryrun1.changes["purged"]["desired"]
+
     ctx = project.deploy(file1, run_as_root=False)
     assert ctx.status == ResourceState.deployed
     assert os.path.exists(test_path_1)
     with open(test_path_1, "r") as fd:
         content = fd.read()
         assert content == "test test test"
+
+    ctx_dryrun2 = project.dryrun(file1)
+    assert not ctx_dryrun2.changes
 
 
 @pytest.mark.parametrize("current_state_purged", [True, False])
@@ -51,7 +59,8 @@ def test_file_purge(project, tmpdir, current_state_purged):
         assert not os.path.exists(test_path_1)
     else:
         with open(test_path_1, 'w+') as f:
-            f.write("test")
+            f.write("test test test")
+        os.chmod(test_path_1, 0o644)
         assert os.path.exists(test_path_1)
 
     project.compile(
@@ -74,6 +83,18 @@ std::ConfigFile(host=host,
 
     file1 = project.get_resource("std::ConfigFile", path=test_path_1)
     assert file1.path == test_path_1
+
+    ctx_dryrun1 = project.dryrun(file1)
+    if current_state_purged:
+        assert not ctx_dryrun1.changes
+    else:
+        assert len(ctx_dryrun1.changes) == 1
+        assert not ctx_dryrun1.changes["purged"]["current"]
+        assert ctx_dryrun1.changes["purged"]["desired"]
+
     ctx = project.deploy(file1, run_as_root=False)
     assert ctx.status == ResourceState.deployed
     assert not os.path.exists(test_path_1)
+
+    ctx_dryrun2 = project.dryrun(file1)
+    assert not ctx_dryrun2.changes
