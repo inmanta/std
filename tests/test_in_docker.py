407c538725d0b5e468092f2ce10d5033c0d40d3f
Andras Kovari <andras.kovari@inmanta.com>
2020-07-08 11:33:03 +0200

    Comment

--- tests/test_in_docker.py
+++ tests/test_in_docker.py
@@ -9,7 +9,7 @@ LOGGER = logging.getLogger(__name__)
 
 @pytest.mark.skipif(
     not os.getenv("INMANTA_TEST_INFRA_SETUP", "false").lower() == "true",
-    reason="Only run when env var is set to true",
+    reason="Only run when test infra environment variable is set to true",
 )
 def test_docker(docker_container):
     LOGGER.debug(f"Running tests in container {docker_container}")
