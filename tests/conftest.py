ec8e747871097811a7d78d8727f78eb984122d13
Yaron Vanhulst <kazaamjt@gmail.com>
2020-07-16 11:59:57 +0200

    Use generic jenkins file (#110)
    * remove jenkins file, add ci file
    
    * minor cleanup
    
    * expose junit as junit_docker
    
    * formatting
    
    * add merging of junit files
    
    * formatting
    
    * formatting
    
    * Split docker junit categories
    
    * formatting
    
    * Update requirements.dev.txt
    
    Co-authored-by: arnaudsjs <2684622+arnaudsjs@users.noreply.github.com>
    
    * Update requirements.txt
    
    Co-authored-by: arnaudsjs <2684622+arnaudsjs@users.noreply.github.com>
    
    * add docstring
    
    Co-authored-by: arnaudsjs <2684622+arnaudsjs@users.noreply.github.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,12 @@
 import os
 import subprocess
 import uuid
+from pathlib import Path
+from typing import Generator
+from xml.etree import ElementTree
 
 import pytest
+from _pytest.fixtures import SubRequest
 
 collect_ignore = []
 if os.getenv("INMANTA_TEST_INFRA_SETUP", "false").lower() == "true":
@@ -17,8 +21,49 @@ if os.getenv("INMANTA_TEST_INFRA_SETUP", "false").lower() == "true":
     collect_ignore += test_modules
 
 
+def merge_to_junit_xml(filename: str, suite: str) -> None:
+    """
+    Takes a junit-xml filename or path to said file.
+    From this file it extracts the testsuite node and adds it to the junit_docker.xml file,
+    in the process it adds a name to the testsuite (the suite param) and
+    changes the classname from tests.* to {suite}.*
+    Finaly, it removes the original file.
+    This is because jenkins was not handleing multiple junit files being exposed well.
+    The classnames are changed so that the tests are grouped by what container they ran in.
+    """
+    junit_docker = Path("junit_docker.xml")
+    if junit_docker.exists():
+        tree = ElementTree.parse(junit_docker)
+        root = tree.getroot()
+        for testsuite in root:
+            if testsuite.get("name", None) == suite:
+                root.remove(testsuite)
+
+        x_tree = ElementTree.parse(filename)
+        x_root = x_tree.getroot()
+        x_root[0].attrib["name"] = suite
+        fix_classname(x_root[0], suite)
+        root.append(x_root[0])
+
+    else:
+        tree = ElementTree.parse(filename)
+        root = tree.getroot()
+        root[0].attrib["name"] = suite
+        fix_classname(root[0], suite)
+
+    tree.write(junit_docker)
+    os.remove(filename)
+
+
+def fix_classname(testsuite: ElementTree.Element, suite: str) -> None:
+    for element in testsuite:
+        element.attrib["classname"] = element.attrib["classname"].replace(
+            "tests.", f"{suite}."
+        )
+
+
 @pytest.fixture(scope="function", params=[7, 8])
-def docker_container(request) -> None:
+def docker_container(request: SubRequest) -> Generator[str, None, None]:
     centos_version = request.param
     pytest_inmanta_dev = os.getenv("PYTEST_INMANTA_DEV", "false")
     print(f"Using the dev index for pytest-inmanta: {pytest_inmanta_dev}")
@@ -35,7 +80,7 @@ def docker_container(request) -> None:
             "--build-arg",
             f"PYTEST_INMANTA_DEV={pytest_inmanta_dev}",
             "-f",
-            f"./dockerfiles/Dockerfile-centos{centos_version}",
+            f"./dockerfiles/centos{centos_version}.Dockerfile",
         ],
         check=True,
     )
@@ -62,10 +107,12 @@ def docker_container(request) -> None:
     print(f"Started container with id {docker_id}")
     yield docker_id
 
+    junit_file = f"junit_centos_{centos_version}.xml"
     subprocess.run(
-        ["sudo", "docker", "cp", f"{docker_id}:/module/std/junit.xml", "junit.xml"],
+        ["sudo", "docker", "cp", f"{docker_id}:/module/std/junit.xml", junit_file],
         check=True,
     )
+    merge_to_junit_xml(junit_file, f"centos-{centos_version}")
     no_clean = os.getenv("INMANTA_NO_CLEAN", "false").lower() == "true"
     print(f"Skipping cleanup: {no_clean}")
     if not no_clean:
