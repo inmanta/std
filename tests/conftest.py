b97d2cbbe75b43b4574830a20a859935b3377f89
Wouter De Borger <wouter.deborger@inmanta.com>
2021-04-26 15:27:50 +0200

    only inmanta packages

--- tests/conftest.py
+++ tests/conftest.py
@@ -81,9 +81,11 @@ def fix_classname(testsuite: ElementTree.Element, suite: str) -> None:
 
 @pytest.fixture
 def pip_lock_file() -> None:
-    """ get all versions of pytest into a freeze file, to make the environment inside docker the same as outside """
-    with open("requirements.freeze", "w") as ff:
+    """ get all versions of inmanta packages into a freeze file, to make the environment inside docker like the one outside """
+    with open("requirements.freeze.all", "w") as ff:
         subprocess.check_call([sys.executable, "-m", "pip", "freeze"], stdout=ff)
+    with open("requirements.freeze", "w") as ff:
+        subprocess.check_call(["grep", "inmanta", "requirements.freeze.all"], stdout=ff)
     yield
 
 
