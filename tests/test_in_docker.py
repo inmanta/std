d9ec5e4998faf6eed91a4650f877fc085d5c1504
Lejoly Florent <florent.lejoly@inmanta.com>
2022-02-01 14:44:20 +0100

    review

--- tests/test_in_docker.py
+++ tests/test_in_docker.py
@@ -21,10 +21,10 @@ import subprocess
 import pytest
 
 
-@pytest.mark.skipif(
-    not os.getenv("INMANTA_TEST_INFRA_SETUP", "false").lower() == "true",
-    reason="Only run when test infra environment variable is set to true",
-)
+# @pytest.mark.skipif(
+#     not os.getenv("INMANTA_TEST_INFRA_SETUP", "false").lower() == "true",
+#     reason="Only run when test infra environment variable is set to true",
+# )
 def test_docker(docker_container):
     print(f"Running tests in container {docker_container}")
     subprocess.run(
